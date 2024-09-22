be405a93236faec1cbf80d52759a5fb5b7867996
ozantalay <ozntalay@hotmail.com>
2024-09-22 23:07:40 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const INITIAL_LIST = [
+  { name: "Domates", value: 55.0 },
+  { name: "Marul", value: 25.5 },
+  { name: "Ekmek", value: 10.99 }
+];
+
+function App() {
+  return <ItemValueList />;
+}
+
+const ItemValueList = () => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
